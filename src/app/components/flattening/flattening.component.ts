998084480d00d9f887a598e28d49f8433d88f3eb
Angel Basto Gonzalez <angelbastogz@gmail.com>
2021-08-12 23:40:20 -0500

    flattening function finished

--- src/app/components/flattening/flattening.component.ts
+++ src/app/components/flattening/flattening.component.ts
@@ -15,28 +15,30 @@ export class FlatteningComponent {
   isCorrect: boolean | undefined;
 
   constructor() {
-    this.input  = [1, [2, [3, 4]], 5];
-    this.expectedResult = [1, 2, 3, 4, 5];
+    this.input  = [1, [2, [3, [4]]], 5, [6, 7]];
+    this.expectedResult = [1, 2, 3, 4, 5, 6, 7];
     this.result = [];
   }
 
   // First implementation
   flatten(array: Array<any>): Array<any> {
-    this.result = array.reduce((a, b, index) => {
-      // The first time, we need to transform a to be an array
-      if (index === 1) {
+    this.result = this.flattenArray(array);
+    this.isCorrect = _.isEqual(this.result, this.expectedResult);
+    return this.result;
+  }
+
+  flattenArray(array: Array<any>): Array<any> {
+    return array.reduce((a, b, index) => {
+      if (index == 1) {
         a = [a];
       }
-      // Each new item, needs to be validate to know if it is an array
       if (Array.isArray(b)) {
-        a = a.concat(b);
+        a = a.concat(this.flattenArray(b));
       } else {
         a.push(b);
       }
       return a;
     });
-    this.isCorrect = _.isEqual(this.result, this.expectedResult);
-    return this.result;
   }
 
 }
